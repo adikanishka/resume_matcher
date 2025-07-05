92ca4a229bb25f3a2b99fabc39d037b38bbbe1d1
adikanishka <adikanishka06@gmail.com>
2025-07-05 19:11:56 +0530

    edited app.py

--- app.py
+++ app.py
@@ -88,6 +88,6 @@ def analyze():
     else:
         return "Invalid file type. Only PDF allowed.", 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render expects this
+    app.run(host="0.0.0.0", port=port)
